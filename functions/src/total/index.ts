39d0395e8b59e726161d00cc7304959ee23b68b9
Joe Lanzon <joelanzon@outlook.com>
2021-03-08 10:53:41 +0000

    dev

--- functions/src/total/index.ts
+++ functions/src/total/index.ts
@@ -4,21 +4,22 @@ admin.initializeApp()
 
 const db = admin.firestore();
 
-export const costCount = functions.firestore
-  .document('Receipts/{ReceiptsId}')
+export const costCount = functions.firestore.document('Receipts/{ReceiptsId}')
   .onCreate(async (snapshot, context) => {
-    const data = snapshot.data()
-    const dataCost = data.cost
+    const ReceiptsCollection = snapshot.data();
     // can track users here
     const runningCost = db.doc('RunningCost/total');
     const runnDoc = await runningCost.get();
     const runnData = runnDoc.data();
 
-    // const next = {
-    //   total: runnData.totalCost + dataCost.cost 
-    // }
+    const next = {
+      total: runnData!.totalCost +  ReceiptsCollection.cost,
+      count: runnData!.count + 1
+    }
 
-    return runningCost.update({
-      totalCost: runnData?.totalCost + dataCost.cost 
+    
+    runningCost.update({
+      next 
     })
+    return next
   });
